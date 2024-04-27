56e4805190674955a891dea77de5f23f54952a7e
ShanderB <s.hander8910@hotmail.com>
2024-04-27 00:20:30 -0300

    Criado função para pegar os dados do usuário no JWT.

--- src/common/validacaoTipoUsuario.ts
+++ src/common/validacaoTipoUsuario.ts
@@ -8,6 +8,7 @@ import { UsuarioTipo } from '../constants';
 interface MinhaJwtPayload extends JwtPayload {
   usuario: string;
   senha: string;
+  id: string;
 }
 
 //TODO jogar em uma interface própria
@@ -45,3 +46,19 @@ export async function validacaoTipoUsuario(req: Request, res: Response, tipoUsua
     return false;
   }
 };
+
+
+export function dadosJWT(req: Request, res: Response, pool: Pool): MinhaJwtPayload | undefined {
+  const authHeader = req.headers.authorization?.split(' ')[1];
+  let dadosUsuario: MinhaJwtPayload | undefined = undefined;
+
+  try {
+    const { usuario, senha, usuarioId } = jwt.verify(authHeader!, process.env.JWT_SECRET!) as MinhaJwtPayload;
+    dadosUsuario = { usuario, senha, id: usuarioId };
+
+  } catch (err) {
+    console.log(err)
+  }
+
+  return dadosUsuario;
+}
