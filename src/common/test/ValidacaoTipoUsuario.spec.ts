2df27648bc1e7f5a0da34aeda8ef033f97fbd04f
ShanderB <s.hander8910@hotmail.com>
2024-04-28 14:16:36 -0300

    Adicionado mais testes para validação de usuário.

--- src/common/test/ValidacaoTipoUsuario.spec.ts
+++ src/common/test/ValidacaoTipoUsuario.spec.ts
@@ -13,22 +13,104 @@ describe('validacaoTipoUsuario', () => {
   jest.spyOn(pg as any, 'Pool').mockImplementation(() => mockPool as any);
 
   describe('validacaoTipoUsuario', () => {
+    const req = {
+      headers: {}
+    } as unknown as Request;
+
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn()
+    } as unknown as Response;
+
+
+    const next: NextFunction = jest.fn();
+
     it('Deve retornar 401 se nenhum cabeçalho de autorização for fornecido', async () => {
-      const req = {
-        headers: {}
-      } as unknown as Request;
 
-      const res = {
-        status: jest.fn().mockReturnThis(),
-        json: jest.fn()
-      } as unknown as Response;
-
-      const next: NextFunction = jest.fn();
 
       await validacaoTipoUsuario(req, res, UsuarioTipo.ORGANIZADOR, mockPool, next);
 
       expect(res.status).toHaveBeenCalledWith(401);
       expect(res.json).toHaveBeenCalledWith({ message: 'Token não fornecido' });
+    });
+
+    it('Deve chamar next e retornar se o token for válido e o usuário tiver permissão', async () => {
+      const req = {
+        headers: {
+          authorization: 'Bearer token',
+        },
+      } as unknown as Request;
+
+      jwt.verify = jest.fn().mockReturnValue({ usuario: 'usuario', senha: 'senha' });
+      mockPool.query.mockResolvedValue({ rows: [{ tipo: UsuarioTipo.ORGANIZADOR }], rowCount: 1 });
+
+      await validacaoTipoUsuario(req, res, UsuarioTipo.ORGANIZADOR, mockPool, next);
+
+      expect(next).toHaveBeenCalled();
+      expect(res.status).toHaveBeenCalled();
+      expect(res.json).toHaveBeenCalled();
+    });
+
+    it('Deve retornar 401 se o token for inválido', async () => {
+      const req = {
+        headers: {
+          authorization: 'Bearer token',
+        },
+      } as unknown as Request;
+
+      jwt.verify = jest.fn().mockImplementation(() => { throw new Error(); });
+
+      await validacaoTipoUsuario(req, res, UsuarioTipo.ORGANIZADOR, mockPool, next);
+
+      expect(res.status).toHaveBeenCalledWith(401);
+      expect(res.json).toHaveBeenCalledWith({ message: 'Token inválido' });
+    });
+
+    it('Deve chamar next e retornar se o token for válido e o usuário tiver o tipo de permissão especificado', async () => {
+      const req = {
+        headers: {
+          authorization: 'Bearer token',
+        },
+      } as unknown as Request;
+
+      jwt.verify = jest.fn().mockReturnValue({ usuario: 'usuario', senha: 'senha' });
+      mockPool.query.mockResolvedValue({ rows: [{ tipo: UsuarioTipo.PARTICIPANTE }], rowCount: 1 });
+
+      await validacaoTipoUsuario(req, res, UsuarioTipo.PARTICIPANTE, mockPool, next);
+
+      expect(next).toHaveBeenCalled();
+      expect(res.status).toHaveBeenCalled();
+      expect(res.json).toHaveBeenCalled();
+    });
+
+    it('Deve retornar 401 se o token for válido mas o usuário não tiver a permissão correta', async () => {
+      const req = {
+        headers: {
+          authorization: 'Bearer token',
+        },
+      } as unknown as Request;
+
+      jwt.verify = jest.fn().mockReturnValue({ usuario: 'usuario', senha: 'senha' });
+      mockPool.query.mockResolvedValue({ rows: [{ tipo: UsuarioTipo.ORGANIZADOR }], rowCount: 1 });
+
+      await validacaoTipoUsuario(req, res, UsuarioTipo.ORGANIZADOR, mockPool, next);
+
+      expect(res.status).toHaveBeenCalledWith(401);
+    });
+
+    it('Deve retornar 401 se o token for válido mas o usuário não tiver a permissão correta', async () => {
+      const req = {
+        headers: {
+          authorization: 'Bearer 123',
+        },
+      } as unknown as Request;
+
+      jwt.verify = jest.fn().mockReturnValue({ usuario: undefined, senha: 'senha' });
+      mockPool.query.mockResolvedValue({ rows: [{ tipo: UsuarioTipo.ORGANIZADOR }], rowCount: 1 });
+
+      await validacaoTipoUsuario(req, res, UsuarioTipo.PARTICIPANTE, mockPool, next);
+      console.log("aaaaaaaaaaaaaaaa", (res.json as any).mock.calls[0][0])
+      expect(res.status).toHaveBeenCalledWith(401);
     });
   });
 
